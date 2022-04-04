40870aba72da9696d26198fcb0e4b6bf2c86c047
Hussein Samy <hussein.samy02@gmail.com>
2022-04-04 03:57:20 +0200

    Day 3 ( Hero and about area  fully finished using grid )

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.sass']
 })
 export class CardComponent implements OnInit {
-  imgSource: string = '';
+  imgSource: string = '../../assets/Photos/0e688544579dc89f02913455bcb5ac92.jpg';
   tags:string[] = ['trousers','bags','shoes'];
   constructor() { }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '790px',
+      'lg': '1120px',
+      'xl': '1410px',
+      '2xl': '1536px'
+    },
     colors: {
       'transparent': 'transparent',
       'primary_red': '#f33a47',
@@ -11,7 +18,12 @@ module.exports = {
     fontFamily: {
       'poppins': ['poppins']
     },
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      gridTemplateRows: {
+        // 'col-custom-layout': 'repeat(12,minmax(900px, 1fr))',
+        'row-custom-layout': '88px repeat(4, minmax(200px, 1fr))'
+      },
+    },
+    plugins: [],
+  }
 }
