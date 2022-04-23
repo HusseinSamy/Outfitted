a2ca56d5b8fae70eaee778e8b9c9acf893a36edc
Hussein Samy <hussein.samy02@gmail.com>
2022-04-24 01:38:29 +0200

    Day 5 (Home page finished, but not 100% responsive)

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.sass']
 })
 export class CardComponent implements OnInit {
-  imgSource: string = '../../assets/Photos/home page photo 1.jpg';
-  tags:string[] = ['trousers','bags','shoes'];
+
+  @Input() !imgSource: string = '../../assets/Photos/home page photo 1.jpg';
+  tags: string[] = ['trousers','bags','shoes'];
+  @Input() backgroundColor: string = '';
+  @Input() shadowColor: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
